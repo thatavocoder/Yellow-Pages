8f133f1f3de0b42cd557a2c7680920ae7dfc033e
Aashka Doshi <aashkadoshi7@gmail.com>
2021-04-15 13:30:21 +0530

    Displaying all scholarship data and removed dropdown from navbar

--- JS/Scholarships.js
+++ JS/Scholarships.js
@@ -1,12 +1,99 @@
 const url = 'https://yp-test-2-new.herokuapp.com'
 
+activeUrl = url + '/api/scholarship/active'
+async function fetchActive() {
+    const res = await fetch(activeUrl);
+    const activeData = await res.json()
+    return activeData;
+}
+fetchActive().then(activeData => {
+    console.log(activeData)
+    for (var i = 0; i < activeData.results.length; i++) {
+
+        var main = document.getElementById('maincontainer')
+        main.setAttribute("class", "row col l10 m9")
+
+        var subcont = document.createElement('div')
+        subcont.setAttribute("class", "col s12 m12 l4")
+        main.appendChild(subcont)
+
+        var card = document.createElement('div')
+        card.setAttribute("class", "card")
+        subcont.appendChild(card)
+
+        var cardcontent = document.createElement('div')
+        cardcontent.setAttribute("class", "card-content")
+        card.appendChild(cardcontent)
+
+        var title = document.createElement('span')
+        title.setAttribute("class", "card-title black-text cardtitle truncate")
+        title.setAttribute("style", "padding-bottom: 1.5vmin; margin-bottom: 2.7vmin; border-bottom: 0.2vmin solid rgb(226, 226, 226); line-height: 3.7vmin; font-size: 3vmin")
+        title.innerHTML = activeData.results[i].title
+        cardcontent.appendChild(title)
+
+        var content = document.createElement('div')
+        content.setAttribute("class", "row")
+        cardcontent.appendChild(content)
+
+        var image = document.createElement('img')
+        image.setAttribute("class", "col s4 m4 l4 offset-m4 offset-s4")
+        image.src = activeData.results[i].img
+        content.appendChild(image)
+
+        if (activeData.results[i].image == null) {
+            image.style.display = "none"
+        }
+
+        var award = document.createElement('p')
+        award.setAttribute("class", "cardaward")
+        award.innerHTML = `Amount: ${activeData.results[i].award}`
+        content.appendChild(award)
+
+        if (activeData.results[i].award == null) {
+            award.innerHTML = "Amount: Information not available"
+            content.appendChild(award)
+        }
+
+        var updated = document.createElement('p')
+        updated.setAttribute("class", "truncate updated")
+        updated.innerHTML = `Updated on : ${activeData.results[i].updated_on}`
+        cardcontent.appendChild(updated)
+
+        if (activeData.results[i].updated_on == null) {
+            updated.innerHTML = "Updated on : Information not available"
+            content.appendChild(updated)
+        }
+
+        var br = document.createElement('br')
+        cardcontent.appendChild(br)
+
+        var deadline = document.createElement('p')
+        deadline.setAttribute("class", "truncate deadline")
+        deadline.innerHTML = `Deadline : ${activeData.results[i].deadline}`
+        cardcontent.appendChild(deadline)
+
+        if (activeData.results[i].deadline == null) {
+            deadline.innerHTML = "Deadline : Information not available"
+            content.appendChild(deadline)
+        }
+
+        var action = document.createElement('div')
+        action.setAttribute("class", "card-action")
+        card.appendChild(action)
+
+        var details = document.createElement('a')
+        details.href = "aboutscholarship.html"
+        details.innerHTML = "View Details"
+        action.appendChild(details)
+    }
+})
+
 async function fetchFilterFields() {
     const filterUrl = url + '/api/scholarship/filterFields'
     const res = await fetch(filterUrl);
     const data = await res.json()
     return data;
 }
-
 fetchFilterFields().then(data => {
     console.log(data)
 
@@ -90,8 +177,21 @@ fetchFilterFields().then(data => {
         genderDD.appendChild(options)
     }
 
-    
+    const submitForm = document.addEventListener('submit', clickSubmit)
 })
+
+function clickSubmit() {
+    const searchForm = document.querySelector('form');
+    const state = document.getElementById('states').selectedIndex;
+    const svalue = state.text
+    console.log(svalue)
+    const category = document.querySelector('category');
+    const classes = document.querySelector('class');
+    const type = document.querySelector('type');
+    const religion = document.querySelector('religion');
+    const course = document.querySelector('course');
+    const gender = document.querySelector('gender');
+}
 
 /*
 
@@ -104,7 +204,7 @@ const religion = document.querySelector('religion');
 const course = document.querySelector('course');
 const gender = document.querySelector('gender');
 
-for (var i = 0; i < resp.results.length; i++) {
+for (var i = 0; i < activeData.results.length; i++) {
 
     var main = document.getElementById('maincontainer')
     main.setAttribute("class", "row col l10 m9")
@@ -124,7 +224,7 @@ for (var i = 0; i < resp.results.length; i++) {
     var title = document.createElement('span')
     title.setAttribute("class", "card-title black-text cardtitle truncate")
     title.setAttribute("style", "padding-bottom: 1.5vmin; margin-bottom: 2.7vmin; border-bottom: 0.2vmin solid rgb(226, 226, 226); line-height: 3.7vmin; font-size: 3vmin")
-    title.innerHTML = resp.results[i].title
+    title.innerHTML = activeData.results[i].title
     cardcontent.appendChild(title)
 
     var content = document.createElement('div')
@@ -133,29 +233,29 @@ for (var i = 0; i < resp.results.length; i++) {
 
     var image = document.createElement('img')
     image.setAttribute("class", "col s4 m4 l4 offset-m4 offset-s4")
-    image.src = resp.results[i].img
+    image.src = activeData.results[i].img
     content.appendChild(image)
 
-    if (resp.results[i].image == null) {
+    if (activeData.results[i].image == null) {
         image.style.display = "none"
     }
 
     var award = document.createElement('p')
     award.setAttribute("class", "cardaward")
-    award.innerHTML = `Amount: ${resp.results[i].award}`
+    award.innerHTML = `Amount: ${activeData.results[i].award}`
     content.appendChild(award)
 
-    if (resp.results[i].award == null) {
+    if (activeData.results[i].award == null) {
         award.innerHTML = "Amount: Information not available"
         content.appendChild(award)
     }
 
     var updated = document.createElement('p')
     updated.setAttribute("class", "truncate updated")
-    updated.innerHTML = `Updated on : ${resp.results[i].updated_on}`
+    updated.innerHTML = `Updated on : ${activeData.results[i].updated_on}`
     cardcontent.appendChild(updated)
 
-    if (resp.results[i].updated_on == null) {
+    if (activeData.results[i].updated_on == null) {
         updated.innerHTML = "Updated on : Information not available"
         content.appendChild(updated)
     }
@@ -165,10 +265,10 @@ for (var i = 0; i < resp.results.length; i++) {
 
     var deadline = document.createElement('p')
     deadline.setAttribute("class", "truncate deadline")
-    deadline.innerHTML = `Deadline : ${resp.results[i].deadline}`
+    deadline.innerHTML = `Deadline : ${activeData.results[i].deadline}`
     cardcontent.appendChild(deadline)
 
-    if (resp.results[i].deadline == null) {
+    if (activeData.results[i].deadline == null) {
         deadline.innerHTML = "Deadline : Information not available"
         content.appendChild(deadline)
     }
--- JS/navbar.js
+++ JS/navbar.js
@@ -2,13 +2,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
 });
-
-var dropdowns = document.querySelectorAll('.dropdown-trigger')
-for (var i = 0; i < dropdowns.length; i++) {
-    M.Dropdown.init(dropdowns[i]);
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
-});
