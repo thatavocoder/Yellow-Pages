3e91dceb9a8c8a80ff8d9a2eb3412bcc7f2493f8
Deekshita Nirmal <deekshitanirmal21@gmail.com>
2021-04-30 02:55:42 +0530

    add: crowd sourcing page
    Adding the crowd sourcing page to the website

---
+++ crowdsourcing.js
@@ -0,0 +1,10 @@
+top.visible_div_id = 'north';
+
+function toggle_visibility(id) {
+    var old_e = document.getElementById(top.visible_div_id);
+    old_e.style.display = 'none';
+    var new_e = document.getElementById(id);
+    console.log('new', new_e, 'block');
+    new_e.style.display = 'block';
+    top.visible_div_id = id;
+}
